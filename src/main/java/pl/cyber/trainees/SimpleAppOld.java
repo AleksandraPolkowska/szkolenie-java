810553f34f46f8ba65c6a7660ef718a3186c5b2c
Aleksandra Polkowska <a.z.polkowska@gmail.com>
2022-07-15 18:08:13 +0200

    dodanie informacji o obiekcie modelpojazdu1 z uzyciem metody set i wyswietlenie go

--- src/main/java/pl/cyber/trainees/SimpleAppOld.java
+++ src/main/java/pl/cyber/trainees/SimpleAppOld.java
@@ -15,8 +15,18 @@ public class SimpleAppOld {
                 2.0, Kolor.valueOf( "ZIELONY"));
         var modelPojazdu3 = new Model ( "VW", "2000", "przód",  "Passat", 30000,
                 2.0, Kolor.valueOf( "NIEBIESKI"));
+        modelPojazdu1.setKolor(Kolor.CZERWONY);
+        modelPojazdu1.setMarka("Skoda");
+        modelPojazdu1.setNazwa("Fabia");
+        modelPojazdu1.setPrzebieg(12000);
+        modelPojazdu1.setPojemnoscSilnika(1.4);
+        modelPojazdu1.setNaped("przód");
+        modelPojazdu1.setRocznik("1990");
+
+
         System.out.println(modelPojazdu1.toString());
         System.out.println(modelPojazdu1);
+        //nie ma znaczenia której metody użyjemy, działa tak samo, ale program wyszarza to.String wiec lepiej to skasować
 
         System.out.println(modelPojazdu2.getMarka());
         System.out.println(modelPojazdu2.getNaped());
