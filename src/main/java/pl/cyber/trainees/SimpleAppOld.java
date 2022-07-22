ce37f241d894002d9a8c625e36fb478c80d78321
Aleksandra Polkowska <a.z.polkowska@gmail.com>
2022-07-22 19:53:20 +0200

    Czyszczenie SimpleAppOld

--- src/main/java/pl/cyber/trainees/SimpleAppOld.java
+++ src/main/java/pl/cyber/trainees/SimpleAppOld.java
@@ -8,20 +8,5 @@ public class SimpleAppOld {
 
     public static void main(String[] args) {
 
-        //Ctrl + P - podpowiadanie w użyciu metod / deklaracji obiektów dostępnych parametrów
-
-        Model modelPojazdu1 = new Model ();
-        var modelPojazdu2 = new Model ( "VW", "2020", "przód",  "Passat", 10000,
-                2.0, Kolor.valueOf( "ZIELONY"));
-        var modelPojazdu3 = new Model ( "VW", "2000", "przód",  "Passat", 30000,
-                2.0, Kolor.valueOf( "NIEBIESKI"));
-        System.out.println(modelPojazdu1.toString());
-        System.out.println(modelPojazdu1);
-
-        System.out.println(modelPojazdu2.getMarka());
-        System.out.println(modelPojazdu2.getNaped());
-        System.out.println(modelPojazdu2.getNazwa());
-
     }
-
 }
